7a09efdd1f0fb2afe0eb4d6bdb733a95d93672c8
soham sarkar <sohamsarkar59@gmail.com>
2024-01-01 20:49:23 +0530

    reformating code for new feature

--- src/components/todoContainer.svelte
+++ src/components/todoContainer.svelte
@@ -1,60 +1,13 @@
 <script>
   import TodoCard from './todoCard.svelte';
   import { tasks } from '../lib/store.js';
-  import { onMount } from 'svelte';
-  import Head from './head.svelte';
+  import { createEventDispatcher } from 'svelte';
 
-  const handleTasksPresent = async () => {
-    try {
-      let tempTasks = [...$tasks];
-
-      if (tempTasks.length > 0) {
-        await Promise.all(
-          tempTasks.map(async (task) => {
-            const data = await add(task.text);
-            task.id = data.id;
-
-            if (task.done) {
-              await update(task.id, 'PUT');
-            }
-          }),
-        );
-      }
-      $tasks = [...new Map([...$tasks, ...tempTasks].map((item) => [item.id, item])).values()];
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const dispatch = createEventDispatcher();
 
   const handleAdd = async (event) => {
     const task = prompt('Enter a new task');
-    const data = await add(task);
-
-    if (data.success) {
-      $tasks = [...$tasks, { id: data.id, text: task, done: false }];
-      return;
-    }
-    $tasks = [...$tasks, { id: Date.now(), text: task, done: false }];
-  };
-
-  const add = async (title) => {
-    try {
-      const res = await fetch('http://localhost:8080/api/v1/task/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title }),
-        withCredentials: true,
-        credentials: 'include',
-      });
-      const data = await res.json();
-
-      return data;
-    } catch (error) {
-      console.error(error);
-      return;
-    }
+    dispatch('add', task);
   };
 
   const handleSelectAll = (event) => {
@@ -62,7 +15,7 @@
 
     (async () => {
       for (let i = 0; i < tasks.length; i++) {
-        await update(tasks[i].id, 'PUT');
+        dispatch('done', [tasks[i].id, 'PUT']);
       }
     })();
   };
@@ -72,32 +25,13 @@
       task.id === event.detail ? { ...task, done: !task.done } : task,
     );
     $tasks.sort((a, b) => a.done - b.done);
-    await update(event.detail, 'PUT');
+    dispatch('done', [event.detail, 'PUT']);
   };
 
   const handleDelete = async (event) => {
     $tasks = $tasks.filter((task) => task.id !== event.detail);
-    await update(event.detail, 'DELETE');
+    dispatch('delete', [event.detail, 'DELETE']);
   };
-
-  const update = async (id, method) => {
-    try {
-      const res = await fetch(`http://localhost:8080/api/v1/task/update/${id}`, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        withCredentials: true,
-        credentials: 'include',
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  onMount(async () => {
-    await handleTasksPresent();
-  });
 </script>
 
 <div class="todo-container">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,7 @@
       console.log(error.message);
     }
   };
+
   const handleIfLogin = async () => {
     try {
       const res = await fetch('http://localhost:8080/api/v1/task/myTask', {
@@ -39,7 +40,59 @@
             { id: data.tasks[i]._id, text: data.tasks[i].title, done: data.tasks[i].completed },
           ];
         }
+        let tempTasks = [...$tasks];
+
+        if (tempTasks.length > 0) {
+          await Promise.all(
+            tempTasks.map(async (task) => {
+              const data = await add(task.text);
+              task.id = data.id;
+
+              if (task.done) {
+                await update(task.id, 'PUT');
+              }
+            }),
+          );
+        }
+        $tasks = [...new Map([...$tasks, ...tempTasks].map((item) => [item.id, item])).values()];
       }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const add = async (title) => {
+    try {
+      const res = await fetch('http://localhost:8080/api/v1/task/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title }),
+        withCredentials: true,
+        credentials: 'include',
+      });
+      const data = await res.json();
+
+      if (data.success) {
+        $tasks = [...$tasks, { id: data.id, text: title, done: false }];
+        return data;
+      }
+      $tasks = [...$tasks, { id: Date.now(), text: title, done: false }];
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const update = async (id, method) => {
+    try {
+      const res = await fetch(`http://localhost:8080/api/v1/task/update/${id}`, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+        credentials: 'include',
+      });
     } catch (error) {
       console.error(error);
     }
@@ -56,7 +109,11 @@
 
 <div class="head"><Head on:logout={handleLogout} /></div>
 <div class="todo">
-  <TodoContainer />
+  <TodoContainer
+    on:add={(e) => add(e.detail)}
+    on:done={(e) => update(...e.detail)}
+    on:delete={(e) => update(...e.detail)}
+  />
 </div>
 
 <style>
